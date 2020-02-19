c308f474a4f45ff1a682f63d681dbd94512513cd
ch3nj <benjaminchen64@gmail.com>
2020-02-19 16:09:25 -0500

    disable

--- Assets/intermittent_interrupt.cs
+++ Assets/intermittent_interrupt.cs
@@ -7,16 +7,19 @@ public class intermittent_interrupt : MonoBehaviour
 
 public static COLORS current_disabled;
 
+public Vector3 curPosition = new Vector3(9.5f ,3.5f, 0);
 public GameObject orange;
 public GameObject yellow;
 public GameObject green;
 public GameObject blue;
+public GameObject disable;
 
 public GameObject current = null;
 private bool invoked = false;
  void Update () {
      if(!invoked && globals.gameStarted){
         InvokeRepeating("DisableRandom", 0f, 3f);  //0s delay, repeat every 10s
+        Instantiate(disable, curPosition, Quaternion.identity);
         invoked = true;
      }
  }
@@ -27,7 +30,6 @@ private bool invoked = false;
         Destroy(current);
         current = null;
     }
-    Vector3 curPosition = new Vector3(9.5f ,3.5f, 0);
     switch (x) {
             case 0:
             current_disabled = COLORS.BLUE;
